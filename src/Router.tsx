9820155ab8038f9fc485d9427c16eda560aa9c4c
김철우 <kimchul8747@daum.net>
2023-08-08 20:49:45 +0900

    feat ::  라우터 설정

---
+++ src/Router.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
