88454b1fa58f3783532efedf7169f54d9a68f57b
김철우 <kimchul8747@daum.net>
2023-08-22 04:13:25 +0900

    feat :: login icon 추가

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,2 +1,4 @@
 export { default as Alram } from "./Alram.svg";
 export { default as ChangeIcon } from "./ChangeIcon.svg";
+export { default as LoginLeft } from "./LoginLeft.svg";
+export { default as LoginRight } from "./LoginRight.svg";
