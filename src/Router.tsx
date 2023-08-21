10267513001fa91b099bafa717e51184e42b58b7
김철우 <kimchul8747@daum.net>
2023-08-22 04:12:44 +0900

    feat :: login page 생성 후 router 적용

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { MainPage } from "./pages/index";
+import { LoginPage, MainPage } from "./pages/index";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/main" element={<MainPage />} />
+        <Route path="" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
