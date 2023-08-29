ddb822c9732c3124617ae4fbc1411a2ec6b5a64b
김철우 <kimchul8747@daum.net>
2023-08-29 20:07:20 +0900

    feat :: login 기능 구현

--- src/component/login/index.tsx
+++ src/component/login/index.tsx
@@ -1,16 +1,49 @@
 import * as S from "./style";
 import { PlanitLogo } from "../../assets/logo";
 import { LoginLeft, LoginRight } from "../../assets/icons";
+import { useState, ChangeEvent } from "react";
+import { postLogin } from "../../utils/apis/login";
 
 const Login = () => {
+  const [loginOption, setLoginOption] = useState({
+    idOption: "",
+    passwordOption: "",
+  });
+  const { idOption, passwordOption } = loginOption;
+  const { mutate: onclickLoginBtn } = postLogin({
+    account_id: idOption,
+    password: passwordOption,
+  });
+
+  const onloginOptionChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setLoginOption({
+      ...loginOption,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <S.LoginArea>
       <S.IconLeft src={LoginLeft} />
       <S.IconRight src={LoginRight} />
       <S.Logo src={PlanitLogo} />
-      <S.Input placeholder="ID" />
-      <S.Input placeholder="Password" />
-      <S.LoginBtn>Login</S.LoginBtn>
+      <S.Input
+        placeholder="ID"
+        name="idOption"
+        onChange={onloginOptionChange}
+      />
+      <S.Input
+        placeholder="Password"
+        name="passwordOption"
+        onChange={onloginOptionChange}
+        type="password"
+      />
+      <S.LoginBtn
+        disabled={idOption === "" || passwordOption === ""}
+        onClick={() => onclickLoginBtn()}
+      >
+        Login
+      </S.LoginBtn>
     </S.LoginArea>
   );
 };
