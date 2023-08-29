6f0df29abda6842598f02da9bcdb1fee1c93a418
김철우 <kimchul8747@daum.net>
2023-08-29 20:08:19 +0900

    setting :: react-toastify 설정

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Router from "./Router";
 import GlobalStyle from "./style/globalStyle";
 import { ThemeProvider } from "styled-components";
 import theme from "./utils/theme/theme";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const queryClient = new QueryClient();
 
@@ -10,6 +12,17 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
+        <ToastContainer
+          position="top-right" // 알람 위치 지정
+          autoClose={3000} // 자동 off 시간
+          hideProgressBar={false} // 진행시간바 숨김
+          closeOnClick // 클릭으로 알람 닫기
+          rtl={false} // 알림 좌우 반전
+          pauseOnFocusLoss // 화면을 벗어나면 알람 정지
+          draggable // 드래그 가능
+          pauseOnHover // 마우스를 올리면 알람 정지
+          theme="light"
+        />
         <Router />
         <GlobalStyle />
       </QueryClientProvider>
