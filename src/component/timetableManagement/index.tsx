0da81d7a37bd3fa766d8cd2ffe23dab25f69a916
김철우 <kimchul8747@daum.net>
2023-09-29 01:50:17 +0900

    feat :: 시간표 관리 페이지 퍼블리싱

---
+++ src/component/timetableManagement/index.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import Sidebar from "./sidebar";
+import SentChangeRequest from "./sentChangeRequest";
+import * as S from "./style";
+import TakeChangeRequest from "./takeChangeRequest";
+
+const TimetableManagement = () => {
+  const [selectTab, setSelectTab] = useState(true);
+  return (
+    <S.Container>
+      <Sidebar selectTab={selectTab} setSelectTab={setSelectTab} />
+      {selectTab ? <TakeChangeRequest /> : <SentChangeRequest />}
+    </S.Container>
+  );
+};
+
+export default TimetableManagement;
