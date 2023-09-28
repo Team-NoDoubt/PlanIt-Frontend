8ccb06455db8fa3d6d88c23ef2e48d1de8c07262
김철우 <kimchul8747@daum.net>
2023-09-29 01:44:00 +0900

    feat :: 계획서 작성 페이지 퍼블리싱

---
+++ src/component/timetableManagement/planWrite/index.tsx
@@ -0,0 +1,116 @@
+import * as S from "./style";
+import {
+  MakeupClassContent,
+  MakeupClassType,
+  ReplaceClassContent,
+  ReplaceClassType,
+} from "../../../constants/timetableManagement";
+import { useState } from "react";
+import { AddList } from "../../../assets/icons";
+
+const PlanWrite = () => {
+  const [replaceClassContents, setReplaceClassContents] = useState([0]);
+  const [makeUpClassContents, setMakeUpClassContents] = useState([0]);
+
+  return (
+    <S.Area>
+      <S.RequestButton>요청하기</S.RequestButton>
+      <S.Container>
+        <S.Wrapper>
+          <S.Header>
+            <span>결 보강 및 수업교체 계획서</span>
+            <S.HeaderText>
+              <S.Teacher>요청교사: 김설우</S.Teacher>
+              <S.Reason>사유: 출장</S.Reason>
+              <hr />
+            </S.HeaderText>
+          </S.Header>
+          <S.MakeupPlanWrapper>
+            <S.PlanText>결 보강 계획서</S.PlanText>
+            <S.PlanTable>
+              <tbody>
+                <tr>
+                  {MakeupClassType.map((item, index) => {
+                    return (
+                      <td style={{ width: `${item.size}%` }} key={index}>
+                        {item.value}
+                      </td>
+                    );
+                  })}
+                </tr>
+              </tbody>
+            </S.PlanTable>
+            <S.PlanTableContent>
+              <tbody>
+                {makeUpClassContents.map(() => {
+                  return (
+                    <tr>
+                      {MakeupClassContent.map((item, index) => {
+                        return (
+                          <td
+                            style={{ width: `${item.size}%`, height: 35 }}
+                            key={index}
+                          >
+                            {item.value}
+                          </td>
+                        );
+                      })}
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </S.PlanTableContent>
+            <S.AddListIconLayout
+              onClick={() => setMakeUpClassContents((prev) => [...prev, 0])}
+            >
+              <img src={AddList} />
+            </S.AddListIconLayout>
+          </S.MakeupPlanWrapper>
+          <S.ReplacePlanWrapper>
+            <S.PlanText>수업교체 계획서</S.PlanText>
+            <S.PlanTable>
+              <tbody>
+                <tr>
+                  {ReplaceClassType.map((item, index) => {
+                    return (
+                      <td style={{ width: `${item.size}%` }} key={index}>
+                        {item.value}
+                      </td>
+                    );
+                  })}
+                </tr>
+              </tbody>
+            </S.PlanTable>
+            <S.PlanTableContent>
+              <tbody>
+                {replaceClassContents.map(() => {
+                  return (
+                    <tr>
+                      {ReplaceClassContent.map((item, index) => {
+                        return (
+                          <td
+                            style={{ width: `${item.size}%`, height: 35 }}
+                            key={index}
+                          >
+                            {item.value}
+                          </td>
+                        );
+                      })}
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </S.PlanTableContent>
+            <S.AddListIconLayout
+              onClick={() => setReplaceClassContents((prev) => [...prev, 0])}
+            >
+              <img src={AddList} />
+            </S.AddListIconLayout>
+          </S.ReplacePlanWrapper>
+        </S.Wrapper>
+      </S.Container>
+    </S.Area>
+  );
+};
+
+export default PlanWrite;
---
+++ src/pages/PlanWritingPage.tsx
@@ -0,0 +1,13 @@
+import Header from "../component/header";
+import PlanWrite from "../component/timetableManagement/planWrite";
+
+const PlanWritingPage = () => {
+  return (
+    <>
+      <Header />
+      <PlanWrite />
+    </>
+  );
+};
+
+export default PlanWritingPage;
