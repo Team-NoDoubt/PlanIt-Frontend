ddc68e48c00e91ab80f8fd71ef783f391b8de1d5
김철우 <kimchul8747@daum.net>
2023-11-01 19:51:04 +0900

    fix :: response type 변경

--- src/utils/apis/timetables/index.ts
+++ src/utils/apis/timetables/index.ts
@@ -22,7 +22,7 @@ export const getTimetableList = (
 
 export const getChangeDetailsList = (grade: number, classNum: number) => {
   return useQuery(["ChangeDetailsList", grade, classNum], async () => {
-    const { data } = await instance.get<Promise<ChangeClassResponse>>(
+    const { data } = await instance.get<ChangeClassResponse>(
       `${router}/changed?grade=${grade}&class=${classNum}`
     );
     return data;
