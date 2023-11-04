98da959501b6345ecd4dc3d21ee1291c0bb50d1d
김철우 <kimchul8747@daum.net>
2023-11-04 21:57:41 +0900

    fix :: uri 수정

--- src/utils/apis/timetables/index.ts
+++ src/utils/apis/timetables/index.ts
@@ -12,7 +12,7 @@ export const getTimetableList = (
     ["timetable", timetableGrade, timetableClass],
     async () => {
       const { data } = await instance.get<Promise<TimetableListResponse>>(
-        `${router}?grade=${timetableGrade}&class=${timetableClass}`
+        `${router}/date?grade=${timetableGrade}&class=${timetableClass}`
       );
       return data;
     },
@@ -23,7 +23,7 @@ export const getTimetableList = (
 export const getChangeDetailsList = (grade: number, classNum: number) => {
   return useQuery(["ChangeDetailsList", grade, classNum], async () => {
     const { data } = await instance.get<ChangeClassResponse>(
-      `${router}/changed?grade=${grade}&class=${classNum}`
+      `${router}/date/changed?grade=${grade}&class=${classNum}`
     );
     return data;
   });
