8e08827ba03876ca85d7019a6a8d155024829139
김철우 <kimchul8747@daum.net>
2023-11-15 19:28:46 +0900

    feat :: header에 토큰 삽입  & status error message 생성

--- src/utils/apis/timetable/index.ts
+++ src/utils/apis/timetable/index.ts
@@ -2,7 +2,7 @@ import { useQuery, useMutation } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 import { toast } from "react-toastify";
 import { instance } from "../index";
-import { useCookies } from "react-cookie";
+import { Cookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
 
 import {
@@ -36,7 +36,6 @@ export const subjectInquiry = (
 ) => {
   return useQuery(["subject", weekOfDate, period, gradeClass], async () => {
     const [grade, classes] = gradeClass.split("-");
-    console.log(gradeClass);
     const date = new Date(weekOfDate).getDay();
     const { data } = await instance.get<SubjectInquiryResponse>(
       `${router}/semester?week-of-date=${date}&period=${period}&grade=${grade}&class=${classes}`
@@ -46,26 +45,43 @@ export const subjectInquiry = (
 };
 
 export const postPlanWrite = (data: PlanWriteType) => {
-  const navigate = useNavigate();
-  const [cookies] = useCookies();
-  const accessToken = cookies.access_token;
+  const navigator = useNavigate();
+  const cookies = new Cookies();
+  const accessToken = cookies.get("access_token");
   return useMutation(
     async () =>
-      instance.post(`${router}/timetables`, data, {
+      await instance.post(`${router}/timetables`, data, {
         headers: {
-          Authorization: `Bearer ${cookies.get(accessToken)}`,
+          Authorization: `Bearer ${accessToken}`,
         },
       }),
     {
       onSuccess: () => {
         toast.success("계획서 작성이 완료되었습니다.");
+        navigator("/SentChangeRequest");
       },
       onError: (err: AxiosError<AxiosError>) => {
         if (err.response) {
           switch (err.response.status) {
+            case 400:
+              toast.error("잘못된 요청입니다. 입력란을 확인해 주세요.");
+              break;
+            case 404:
+              toast.error("찾을 수 없습니다.");
+              break;
+            case 409:
+              toast.error("이미 작성하신 계획서입니다.");
+              break;
+            case 429:
+              toast.error("요청을 너무 많이 보냈습니다.");
+              break;
+            case 500:
+              toast.error("개발자에게 문의해 주세요.");
+              break;
           }
+        } else {
+          toast.error("개발자에게 문의해 주세요.");
         }
-        alert("asdf");
       },
     }
   );
